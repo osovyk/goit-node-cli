8429740fc8b7dbba8e1e2cda87e11b9b43441d7d
Roman Osovyk <r.osovyk@gmail.com>
2025-11-02 19:38:46 +0200

    Add contact management functionality and update README

---
+++ index.js
@@ -0,0 +1,41 @@
+import {program} from "commander";
+import {addContact, getContactById, listContacts, removeContact,} from "./contacts.js";
+
+program
+    .option("-a, --action <type>", "choose action")
+    .option("-i, --id <type>", "user id")
+    .option("-n, --name <type>", "user name")
+    .option("-e, --email <type>", "user email")
+    .option("-p, --phone <type>", "user phone");
+
+program.parse();
+const options = program.opts();
+
+async function invokeAction({action, id, name, email, phone}) {
+    switch (action) {
+        case "list": {
+            const contacts = await listContacts();
+            console.table(contacts);
+            break;
+        }
+        case "get": {
+            const contact = await getContactById(id);
+            console.log(contact);
+            break;
+        }
+        case "add": {
+            const contact = await addContact(name, email, phone);
+            console.log(contact);
+            break;
+        }
+        case "remove": {
+            const contact = await removeContact(id);
+            console.log(contact);
+            break;
+        }
+        default:
+            console.warn("\x1B[31m Unknown action type!");
+    }
+}
+
+invokeAction(options);
